b5c5c167e853a0ddb5ca2837cc0d14b637493418
inesktari <135965289+inesktari@users.noreply.github.com>
2024-03-06 07:49:40 +0100

    Change image and text on click

--- assets/script.js
+++ assets/script.js
@@ -18,14 +18,54 @@ const slides = [
   },
 ];
 
+let i = 0;
+let j = 0;
+
+console.log("i=", i, " et j=", j);
+
+function ChangeSlide(i) {
+  document.querySelector(
+    ".banner-img"
+  ).src = `assets/images/slideshow/${slides[i].image}`;
+  document.querySelector(".texte").innerHTML = `${slides[i].tagLine}`;
+}
+
+// function ChangeDot(j) {
+//   let dot = document.querySelectorAll(".dot");
+
+//   dot.classList.add(".dot_selected");
+// }
+
 // Event listeners sur les fleches
 let gch = document.querySelector(".arrow_left");
 let dte = document.querySelector(".arrow_right");
 
 dte.addEventListener("click", () => {
   console.log("clicks sur la flèche droite");
+  if (i >= 0 && i < slides.length - 1) {
+    ChangeSlide(i + 1);
+    // ChangeDot(i + 1);
+    i++;
+    console.log("i=", i, " et j=", j);
+  } else {
+    i = 0;
+    ChangeSlide(i);
+    // ChangeDot(i);
+    console.log("i=", i, " et j=", j);
+  }
 });
 
 gch.addEventListener("click", () => {
   console.log("clicks sur la flèche gauche");
+  if (i > 0 && i <= slides.length) {
+    ChangeSlide(i - 1);
+    // ChangeDot(i - 1);
+    i--;
+    console.log("i=", i, " et j=", j);
+  } else {
+    i = slides.length - 1;
+    ChangeSlide(i);
+    // ChangeDot(i);
+    console.log("i=", i, " et j=", j);
+  }
 });
